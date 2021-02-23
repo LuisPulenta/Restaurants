9ab18e4d6b44c4fc3029404a62c292d3757bedda
Luis Nuñez <luisalbertonu@gmail.com>
2021-02-23 12:07:17 -0300

    Fin video 50

--- components/account/InfoUser.js
+++ components/account/InfoUser.js
@@ -2,14 +2,39 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements'
 
+import { updateProfile, uploadImage } from '../../utils/actions'
+import { loadImageFromGallery } from '../../utils/helpers'
+
 export default function InfoUser({user}) {
-    console.log(user)
+    
+    const changePhoto = async() => {
+        const result = await loadImageFromGallery([1, 1])
+        if (!result.status) {
+            return
+        }
+        setLoadingText("Actualizando imagen...")
+        setLoading(true)
+        const resultUploadImage = await uploadImage(result.image, "avatars", user.uid)
+        if (!resultUploadImage.statusResponse) {
+            setLoading(false)
+            Alert.alert("Ha ocurrido un error al almacenar la foto de perfil.")
+            return
+        }
+        const resultUpdateProfie = await updateProfile({ photoURL: resultUploadImage.url })
+        setLoading(false)
+        if (resultUpdateProfie.statusResponse) {
+            setPhotoUrl(resultUploadImage.url)
+        } else {
+            Alert.alert("Ha ocurrido un error al actualizar la foto de perfil.")
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Avatar
                 rounded
                 size="xlarge"
-                
+                onPress={changePhoto}
                 source={
                     user.photoUrl 
                         ? { uri: photoUrl }
