c3de757091a131db4d850cc5868618e65e9c51f5
Luis Nuñez <luisalbertonu@gmail.com>
2021-03-19 11:21:20 -0300

    Fin video 102

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -65,7 +65,7 @@ export default function Navigation() {
                 <Tab.Screen 
                 name="top-restaurants"
                 component={TopRestaurantsStack}
-                options={{title:"Top 5"}}
+                options={{title:"Top 10"}}
                 />
                 <Tab.Screen 
                 name="search"
